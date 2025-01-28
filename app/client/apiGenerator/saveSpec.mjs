5248eeafaa03b1162da975c61bd61d90bcdd9a9a
German <thewhitehaven04@gmail.com>
2025-01-28 18:16:27 +0100

    Add logging for API specification fetcher

--- app/client/apiGenerator/saveSpec.mjs
+++ app/client/apiGenerator/saveSpec.mjs
@@ -1,24 +1,26 @@
-import * as fs from "node:fs";
+import * as fs from "node:fs"
 
-const SAVE_PATH = "/app/client/apiGenerator/generated";
+const SAVE_PATH = "/app/client/apiGenerator/generated"
 
 export const saveOpenApiSpec = async () => {
     // todo fix
-    const baseUrl = process.env.API_ROOT_URL ?? "http://localhost:8000";
-    const apiDirectory = process.cwd() + SAVE_PATH;
+    const baseUrl = process.env.API_ROOT_URL ?? "http://localhost:8000"
+    const apiDirectory = process.cwd() + SAVE_PATH
 
     if (!fs.existsSync(apiDirectory)) {
-        fs.mkdirSync(apiDirectory);
+        fs.mkdirSync(apiDirectory)
     }
 
-    fetch(`${baseUrl}/openapi.json`)
+    const path = `${baseUrl}/openapi.json`
+    console.log('Fetching API specification from: ', path)
+    fetch(path)
         .then((res) => res.text())
         .then((text) => {
-            fs.writeFileSync(`${apiDirectory}/openapi.json`, text);
+            fs.writeFileSync(`${apiDirectory}/openapi.json`, text)
         })
         .catch(() => {
-            throw new Error("Unable to retrieve API specification data");
-        });
-};
+            throw new Error("Unable to retrieve API specification data")
+        })
+}
 
-saveOpenApiSpec();
+saveOpenApiSpec()
