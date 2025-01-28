42ba601d0cea1d9083ae791f8365218c215ca0fa
German <thewhitehaven04@gmail.com>
2025-01-28 22:35:22 +0100

    Change relative route paths to absolute

--- app/routes.ts
+++ app/routes.ts
@@ -2,12 +2,12 @@ import { type RouteConfig, index, layout, prefix, route } from "@react-router/de
 
 export default [
     ...prefix("year/:year", [
-        layout("./routes/Layout.tsx", [
-            index("./routes/App.tsx"),
-            route("event/:event/session/:session", "./routes/Session/index.tsx", [
-                index("./routes/Session/Results/index.tsx"),
-                route("laps", "./routes/Session/Laps/index.tsx"),
-                route("laps/telemetry", "./routes/Session/Telemetry/index.tsx"),
+        layout("routes/Layout.tsx", [
+            index("routes/App.tsx"),
+            route("event/:event/session/:session", "routes/Session/index.tsx", [
+                index("routes/Session/Results/index.tsx"),
+                route("laps", "routes/Session/Laps/index.tsx"),
+                route("laps/telemetry", "routes/Session/Telemetry/index.tsx"),
             ]),
         ]),
     ]),
