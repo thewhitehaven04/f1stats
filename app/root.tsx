5223fde1e52950767e70147995909bb88d9eecd0
German <thewhitehaven04@gmail.com>
2025-01-29 00:32:36 +0100

    Fix routing for build process

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,10 @@
-import { isRouteErrorResponse, Meta, Outlet, redirect, Scripts, ScrollRestoration } from "react-router"
+import { isRouteErrorResponse, Meta, Outlet, Scripts, ScrollRestoration } from "react-router"
 
 import type { Route } from "./+types/root"
 import "./app.css"
-import type { ReactNode } from "react"
 import { QueryClientProvider } from "@tanstack/react-query"
-import { DEFAULT_YEAR, queryClient } from "~/config"
-
-export async function loader(props: Route.LoaderArgs) {
-    if (!props.params.year) {
-        return redirect(`/year/${DEFAULT_YEAR}`)
-    }
-}
+import { queryClient } from "~/config"
+import type { ReactNode } from 'react'
 
 export function Layout({ children }: { children: ReactNode }) {
     return (
--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,9 @@
 import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes"
 
 export default [
-    ...prefix("year/:year", [
-        layout("routes/Layout.tsx", [
-            index("routes/Application.tsx"),
+    layout("routes/Layout.tsx", [
+        index("routes/Application.tsx"),
+        ...prefix("year/:year", [
             route("event/:event/session/:session", "routes/Session/index.tsx", [
                 index("routes/Session/Results/index.tsx"),
                 route("laps", "routes/Session/Laps/index.tsx"),
