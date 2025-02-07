e1547e1563d23755069671fcb09ef631a861a9ba
German <thewhitehaven04@gmail.com>
2025-02-07 03:11:24 +0100

    remove prerender (test)

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config"
 const PRERENDERED_YEARS = [2022, 2023, 2024]
 export default {
     ssr: true,
-    async prerender({ getStaticPaths }) {
-        return [...getStaticPaths(), ...PRERENDERED_YEARS.map((season) => `/year/${season}`)]
-    },
+    // async prerender({ getStaticPaths }) {
+    //     return [...getStaticPaths(), ...PRERENDERED_YEARS.map((season) => `/year/${season}`)]
+    // },
 } satisfies Config
