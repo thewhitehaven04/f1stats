147bc0fca0751d99fc831e3364f1cc1c78aa8c59
German <thewhitehaven04@gmail.com>
2025-02-10 19:53:06 +0100

    Disable dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,9 @@ import type { Config } from "tailwindcss"
 
 export default {
     content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+    daisyui: {
+        themes: ["light"],
+    },
     theme: {
         extend: {
             fontFamily: {
