ea64a6b65d59158c644df253e202298b43fe5dda
German <thewhitehaven04@gmail.com>
2025-02-11 21:46:25 +0100

    Add prerendering of races

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,26 +1,27 @@
 import type { Config } from "@react-router/dev/config"
+import { yearEventsSeasonYearGet } from "./app/client/generated"
+import { ApiClient } from "./app/client"
 
-// async function getPrerenderedRoutes() {
-//     const events = (
-//         await yearEventsSeasonYearGet({
-//             client: ApiClient,
-//             throwOnError: true,
-//             path: {
-//                 year: 2024,
-//             },
-//         })
-//     ).data
-//     return events.slice(1, 5).flatMap((event) => {
-//         return [
-//             `/year/2024/event/${event.EventName}/session/${event.Session4}`,
-//             `/year/2024/event/${event.EventName}/session/${event.Session5}`,
-//         ]
-//     })
-// }
+async function getPrerenderedRoutes() {
+    const events = (
+        await yearEventsSeasonYearGet({
+            client: ApiClient,
+            throwOnError: true,
+            path: {
+                year: 2024,
+            },
+        })
+    ).data
+    return events.slice(1).flatMap((event) => {
+        return [
+            `/year/2024/event/${event.EventName}/session/${event.Session5}`,
+        ]
+    })
+}
 
 export default {
     ssr: true,
     async prerender() {
-        return ["/year/2021", "/year/2022", "/year/2023", "/year/2024"]
+        return ["/year/2021", "/year/2022", "/year/2023", "/year/2024", ...(await getPrerenderedRoutes())]
     },
 } satisfies Config
