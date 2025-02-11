6620c9a19c84e930cf45816f699112cab5d16f95
German <thewhitehaven04@gmail.com>
2025-02-11 02:02:23 +0100

    Update prerendering

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,28 @@
 import type { Config } from "@react-router/dev/config"
+import { ApiClient } from './app/client/index'
+import { yearEventsSeasonYearGet } from './app/client/generated'
+
+async function getPrerenderedRoutes() {
+    const events = (
+        await yearEventsSeasonYearGet({
+            client: ApiClient,
+            throwOnError: true,
+            path: {
+                year: 2024,
+            },
+        })
+    ).data
+    return events.slice(1).flatMap((event) => {
+        return [
+            `/year/2024/event/${event.OfficialEventName}/session/${event.Session4}`,
+            `/year/2024/event/${event.OfficialEventName}/session/${event.Session5}`,
+        ]
+    })
+}
 
 export default {
     ssr: true,
     async prerender() {
-        return ['/year/2021', '/year/2022', '/year/2023', '/year/2024']
+        return ["/year/2021", "/year/2022", "/year/2023", "/year/2024", ...(await getPrerenderedRoutes())]
     },
 } satisfies Config
